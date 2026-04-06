5bc633fac3cb9bf9bb22eb261fa50190ece8e3ad
Shorya Aggarwal <63948@gbpuat.ac.in>
2026-04-06 17:40:11 +0530

    Create shopping_cart.py
    This program helps you to add items according to your choice in the cart, it then prints the list of all items you entered along with the total amount

---
+++ shopping_cart.py
@@ -0,0 +1,24 @@
+#shopping cart program
+try:
+    foods=[]
+    total=0
+    print("Welcome to FalBazaar🥳🙏...")
+    print("Add your products into the cart and enter q for quitting.")
+    while True:
+        cart=str(input("Enter the food item you want to add to cart:"))
+        if (cart.lower()=='q'):
+            print("checking out....")
+            break
+        else:
+            price=float(input("Enter the price in $ of the item:"))
+            total+=price
+            foods.append(cart)
+    if len(foods)==0:
+        print("Your cart is empty!!")
+    else:
+        print("-------Your Cart-------")
+        print(foods)
+    print(f"Your Total is ${total:.2f} \n Please visit our store again🤗!!")
+except Exception as e:
+    print(e)
+    print("Enter the inputs correctly -_- ")
