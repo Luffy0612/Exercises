51853db3f5aa3a7665a471511fa408ee99eac0fc
Shorya Aggarwal <63948@gbpuat.ac.in>
2026-04-06 16:49:27 +0530

    Create countdown_timer.py
    Just learned a module named 'time' and build a countdown timer program using it

---
+++ countdown_timer.py
@@ -0,0 +1,10 @@
+#Countdown timer program
+import time
+Time=int(input("Enter the time in seconds:"))
+for x in range(Time,0,-1):
+    seconds=x%60
+    minutes=int(x/60)%60
+    hours=int(x/3600)%60
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("Times UP!!")
